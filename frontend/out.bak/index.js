95271c11e8ab5593e57d339d36edabf0c3a0ec2e
Ilya <godlikeSwan@gmail.com>
2022-08-31 18:41:31 +0300

    restruct 2

--- frontend/out.bak/index.js
+++
@@ -1,4 +0,0 @@
-import GameServer from "./game-server.js";
-const gameServer = new GameServer();
-gameServer.game.rooms.addRoom({ name: 'bara bara bere bere', maxPlayers: 5 });
-gameServer.start();
--- frontend/out.old.bak/game.js
+++
@@ -1,7 +0,0 @@
-import RoomList from './room-list.js';
-export default class Game {
-    rooms;
-    constructor() {
-        this.rooms = new RoomList();
-    }
-}
